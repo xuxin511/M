48a4b20f77d408b545ba94db206ecafe7c0b02d2
123 <ymh090510223>
2017-11-19 16:52:58 +0800

    2017111902

--- app/src/main/java/com/xx/chinetek/mitsubshi/Bulkupload/bulkuploadBarcodeDetail.java
+++ app/src/main/java/com/xx/chinetek/mitsubshi/Bulkupload/bulkuploadBarcodeDetail.java
@@ -1,6 +1,10 @@
 package com.xx.chinetek.mitsubshi.Bulkupload;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -9,12 +13,15 @@ import com.xx.chinetek.adapter.Exception.ExceptionScanbarcodeAdapter;
 import com.xx.chinetek.chineteklib.base.BaseActivity;
 import com.xx.chinetek.chineteklib.base.BaseApplication;
 import com.xx.chinetek.chineteklib.base.ToolBarTitle;
+import com.xx.chinetek.chineteklib.util.dialog.MessageBox;
 import com.xx.chinetek.method.DB.DbDnInfo;
 import com.xx.chinetek.mitsubshi.R;
 import com.xx.chinetek.model.DN.DNDetailModel;
+import com.xx.chinetek.model.DN.DNModel;
 import com.xx.chinetek.model.DN.DNScanModel;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
@@ -36,6 +43,7 @@ public class bulkuploadBarcodeDetail extends BaseActivity {
     BarcodeItemAdapter barcodeItemAdapter;
     ArrayList<DNScanModel> dnScanModels;
     DNDetailModel dndetailmodel;
+    DNModel dnModel;
 
     @Override
     protected void initViews() {
@@ -48,6 +56,7 @@ public class bulkuploadBarcodeDetail extends BaseActivity {
     protected void initData() {
         super.initData();
         dndetailmodel=getIntent().getParcelableExtra("DNdetailModel");
+        dnModel=getIntent().getParcelableExtra("DNModel");
         GetBarcodeDetailList();
     }
 
@@ -65,5 +74,55 @@ public class bulkuploadBarcodeDetail extends BaseActivity {
         ArrayList<DNScanModel> dnScanModels =new ArrayList<>();
         dnScanModels = DbDnInfo.getInstance().GetLoaclDNScanModelDN(dndetailmodel.getAGENT_DN_NO(),dndetailmodel.getGOLFA_CODE(),dndetailmodel.getLINE_NO());
         return dnScanModels;
+    }
+
+
+    private int clickposition=-1;
+    @Event(value = R.id.lsv_DeliveryScan,type = AdapterView.OnItemClickListener.class)
+    private void lsvDeliveryScanonItemClick(AdapterView<?> parent, View view, int position, long id) {
+        DNScanModel Modelcheck= (DNScanModel)barcodeItemAdapter.getItem(position);
+        if(Modelcheck.getSERIAL_NO().indexOf("数量")!=-1){
+            return;
+        }
+        clickposition=position;
+        new AlertDialog.Builder(context).setCancelable(false).setTitle("提示").setIcon(android.R.drawable.ic_dialog_info).setMessage("是否删除扫描记录？\n")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO 自动生成的方法
+                        //删除扫描记录，改变明细数量
+                        if(clickposition==-1){
+                            MessageBox.Show(context,"请先选择操作的行！");
+                            return;
+                        }
+                        DNScanModel Model= (DNScanModel)barcodeItemAdapter.getItem(clickposition);
+                        if(DbDnInfo.getInstance().DELscanbyserial(Model.getAGENT_DN_NO(),Model.getGOLFA_CODE(),Model.getLINE_NO(),Model.getSERIAL_NO(),"")){
+                            //判断剩余的扫描数量
+                            Integer lastNum=DbDnInfo.getInstance().GetLoaclDNScanModelDNNum(dndetailmodel.getAGENT_DN_NO(),dndetailmodel.getGOLFA_CODE(),dndetailmodel.getLINE_NO());
+                            if(DbDnInfo.getInstance().UpdateDetailNum(dndetailmodel.getAGENT_DN_NO(),dndetailmodel.getGOLFA_CODE(),dndetailmodel.getLINE_NO(),lastNum,dnModel.getDN_SOURCE())){
+                                if(DbDnInfo.getInstance().GetLoaclDNScanModelDNNumbyDNNO(dndetailmodel.getAGENT_DN_NO())==0){
+                                    //需要改变主表状态
+//                                    DNModel modeldn=dnModel;
+//                                    modeldn.setSTATUS(1);
+                                    if(DbDnInfo.getInstance().UpdateDNmodelState(dndetailmodel.getAGENT_DN_NO(),"1","",dnModel.getDN_SOURCE())){
+
+                                    }else{
+                                        MessageBox.Show(context,getString(R.string.Error_del_dnmodel));
+                                        return;
+                                    }
+                                }
+                                MessageBox.Show(context,getString(R.string.Msg_del_success));
+                                txtScanQty.setText("扫描数量："+(dndetailmodel.getSCAN_QTY()-1));
+                                GetBarcodeDetailList();
+
+                            }else{
+                                MessageBox.Show(context,getString(R.string.Error_del_dnmodeldetail));
+                                return;
+                            }
+                        }else{
+                            MessageBox.Show(context,getString(R.string.Error_del_dnmodelbarcode));
+                        }
+                    }
+                }).setNegativeButton("取消", null).show();
     }
 }
