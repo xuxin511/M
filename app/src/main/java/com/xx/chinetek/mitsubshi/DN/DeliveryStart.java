1168c2a2224bc5f8a592c02790e1a632fcef8dd6
123 <ymh090510223>
2017-11-29 15:03:01 +0800

    2017112901

--- app/src/main/java/com/xx/chinetek/mitsubshi/DN/DeliveryStart.java
+++ app/src/main/java/com/xx/chinetek/mitsubshi/DN/DeliveryStart.java
@@ -148,11 +148,11 @@ public class DeliveryStart extends BaseActivity {
             if (customModel == null && partnerItemAdapter.getCount() == 1) {
                 customModel = (CustomModel) partnerItemAdapter.getItem(0);
             }
-            if (customModel == null) {
-                MessageBox.Show(context, getString(R.string.Msg_NoSelect_CusCode));
-                CommonUtil.setEditFocus(edtContentText);
-                return;
-            }
+//            if (customModel == null) {
+//                MessageBox.Show(context, getString(R.string.Msg_NoSelect_CusCode));
+//                CommonUtil.setEditFocus(edtContentText);
+//                return;
+//            }
         }
         dnTypeModel.setCustomModel(customModel);
         dnTypeModel.setDNCusType(customModel.getPARTNER_FUNCTION().equals("Z3")?1:0);
